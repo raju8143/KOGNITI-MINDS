6c6c4e47d9411c55e7d1cbb4dbd7aff4f44b41a6
raju8143 <165926229+raju8143@users.noreply.github.com>
2025-09-23 10:55:30 +0530

    Add files via upload

---
+++ Task-4 SEO & Analytics Integration/index.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+<main>
+<h1>Welcome to Kogniti Minds</h1>
+<img src="https://www.startinup.up.gov.in/crm/assets/user/images/Documents/Startup/A_STARTUP_UP_UPLC_000038664/startup_logo/174764289222058.png" alt="Kogniti Minds Company Logo">
+
+<p>Explore our services, internships, and more.</p>
+<img src="https://d8it4huxumps7.cloudfront.net/lambda-pdfs/opportunity-bannerImages/1757923653.png" alt="Home page banner showing internships and services">
+</main>
+<?php include 'footer.php'; ?>
